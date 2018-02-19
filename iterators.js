0443ff438f1c00175e6bd0436a4e0bccad9514a5
hoodielive <hoodielive@gmail.com>
2018-02-18 20:21:34 -0500

    iterators

---
+++ iterators.js
@@ -0,0 +1,15 @@
+array = [1,2,3,4,5,6,7];
+
+function iteratoroverstuff(array) {
+    var nextIndex = 0;
+
+    return {
+        next: function() {
+            return nextIndex < array.length?
+                { value: array[nextIndex++], done: false } :
+                { done: true };
+        }
+    };
+}
+
+iteratoroverstuff();
