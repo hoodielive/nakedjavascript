f2e502b45b05bd64a9559751907439e61140da9e
hoodielive <hoodielive@gmail.com>
2018-02-18 20:26:35 -0500

    iterations - in javascript

--- iterators.js
+++ iterators.js
@@ -1,8 +1,7 @@
-array = [1,2,3,4,5,6,7];
+const array = [1,2,3,4,5,6,7];
 
 function iteratoroverstuff(array) {
     var nextIndex = 0;
-
     return {
         next: function() {
             return nextIndex < array.length?
