95c74e93eb9cdb8d1c7d81b522b6f9ba520f37bc
dotecha <apokueo@gmail.com>
2018-02-24 09:38:44 -0500

    moved code to helper-functions

---
+++ helper-functions/iterators.js
@@ -0,0 +1,16 @@
+const array = [1,2,3,4,5,6,7];
+
+function iteratoroverstuff(array) {
+    var nextIndex = 0;
+    return {
+        next: function() {
+            return nextIndex < array.length?
+                { value: array[nextIndex++], done: false } :
+                { done: true };
+        }
+    };
+}
+
+var iteratenow = iteratoroverstuff(array);
+console.log(iteratenow.next().value);
+console.log(iteratenow.next().done);
