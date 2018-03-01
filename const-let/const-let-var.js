1e5c8d86d9abeff04bcbcd7b0c7b9b76fef1aa2a
hoodielive <hoodielive@gmail.com>
2018-03-01 18:47:08 -0500

    Working through const-lets - boring

---
+++ const-let/const-let-var.js
@@ -0,0 +1,9 @@
+// all features in es6 can be put into 2 categories: new ideas,constructs and features to the language and syntatic sugars
+
+let color = 'red'; // the value will change over time
+
+const models = '<>' // the value should not change
+
+
+const name = 'larry' // don't expect your name to change
+
