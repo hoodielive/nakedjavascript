4dbc13d9b39156e3ab5fdc2294992089efe8ee86
hoodielive <hoodielive@gmail.com>
2018-02-28 21:02:08 -0500

    Concat, filter and forEach

--- arrays/concatenation.js
+++ arrays/concatenation.js
@@ -3,3 +3,14 @@ var numeric = [1,2,3,4];
 
 alpha.concat(numeric);
 console.log(alpha.concat(numeric));
+
+var num1 = [[1]];
+var num2 = [2, [3]];
+
+var nums = num1.concat(num2);
+
+console.log(nums);
+
+num1[0].push(4);
+
+console.log(nums);
---
+++ arrays/filter.js
@@ -0,0 +1,15 @@
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+const anotheresult = words.filter(function(word) {
+    return word.length > 8
+    var astra = word.length > 8;
+
+    astra.forEach(function(wordsandstuff) {
+       return wordsandstuff;
+    });
+});
+
+console.log(result);
+console.log(anotheresult);
