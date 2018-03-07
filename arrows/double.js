6482154a17b1d0650b4838a8154a89a2b27b0f96
hoodielive <hoodielive@gmail.com>
2018-03-07 16:02:00 -0500

    Make corrections to double.js

--- arrows/double.js
+++ arrows/double.js
@@ -1,2 +1,9 @@
 // if you have a single argument, you do not have to use the () and also if you have a single expression then put it on this line 
+
 const double = number => 2 * number;  
+double(8); 
+
+// when you don't have any arguments, you still need parantheses 
+
+const double2 = () => 2 * number; 
+double2(8)
