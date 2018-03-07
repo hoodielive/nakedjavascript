36de2a3b20db847e48ccdf6e4988ef701e8defcb
hoodielive <hoodielive@gmail.com>
2018-03-07 17:35:02 -0500

    Order of Destructuring - simplify code

---
+++ destructuring/practicum.js
@@ -0,0 +1,16 @@
+function signup({username, password, email, dob, city}) {
+  // create new user  
+}; 
+
+
+const user = { 
+    username: 'myName', 
+    password: 'myPassword', 
+    email: 'myExample@example.com', 
+    dob: '5/25/1982',
+    city: 'Florida'
+} 
+
+signup(user); 
+
+
