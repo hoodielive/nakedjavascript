9a8e2565f9ec49f4f0cc6f55cc4ed01a0b8900f9
hoodielive <hoodielive@gmail.com>
2018-03-07 16:49:51 -0500

    spread and rest operator is awesome

---
+++ restandspreadoperators/_rest-spread-ops.js
@@ -0,0 +1,7 @@
+const defaultColors = ['red', 'green']; 
+
+const userFavoriteColors = ['orange', 'yellow']; 
+
+
+//defaultColors.concat(userFavoriteColors); LOL or...  
+[ ...defaultColors, ...userFavoriteColors ]; // this is very dope imo
