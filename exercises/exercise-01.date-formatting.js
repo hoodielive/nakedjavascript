49409dc3577b0afa57d969b98e409c4b0d83e105
hoodielive <hoodielive@gmail.com>
2018-03-10 16:19:10 -0500

    Execersises

---
+++ exercises/exercise-01.date-formatting.js
@@ -0,0 +1,10 @@
+/**
+1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+Sample Output : Today is : Friday. 
+Current time is : 4 PM : 50 : 22
+**/
+
+
+var date_format = new Date()
+
+console.log("Today is: " + date_format)
