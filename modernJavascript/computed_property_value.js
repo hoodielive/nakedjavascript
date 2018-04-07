f9d464e1ca881e5e42f63f393e2c28772c1d7f7d
hoodielive <hoodielive@gmail.com>
2018-04-07 07:29:16 -0500

    computed property

---
+++ modernJavascript/computed_property_value.js
@@ -0,0 +1,11 @@
+// Another shorthand technique
+
+var profession = 'journalism'; 
+
+var professional = {
+	name: 'John doe',
+	address : '12 Aethyr lane'
+	[profession] : {
+		experience: '12 years',
+		retirement
+	}
