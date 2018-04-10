31d54e3df3c59a587fef909eaa0cfdc8dfa2b745
hoodielive <hoodielive@gmail.com>
2018-04-10 19:50:50 -0400

    Creating my own filter function, that is not working lol

--- functions/javascript_functions_array.js
+++ functions/javascript_functions_array.js
@@ -1,10 +1,27 @@
-const arr = ['Vincent', 'Margaret', 'Amber', 'Destiny', 'Vincent Sr', 'Evan']; 
+var arr1 = [{
+	FirstBorn : 'Vincent',
+	Mother: 'Margaret', 
+	SecondBorn: 'Amber',
+	Youngin: 'Destiny', 
+	Father: 'Vincent Sr', 
+	KneeBaby: 'Evan'
+}]; 
 
-function curve(arr, amount) {
-	arr.forEach(function(array) { 
+console.log(typeof(arr1));
 
-		console.log(array); 
-	})
+function filterer(array) { 
+	for (var i = 0; i < array.length; i++) { 
+		console.log(array) } 
+} 
+
+filterer(arr1); 
+
+var arr = filterer(); 
+
+function curve(arr) {
+	arr.filterer(function(array) { 
+		return array[arr] === KneeBaby; 	
+	}); 
 }
 
-curve(arr, 0) 
+curve(arr1) 
