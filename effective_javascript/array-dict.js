f9f8d8652cde2358373cce299685a14bcdaf3a7b
hoodielive <hoodielive@gmail.com>
2018-04-19 15:10:01 -0400

    Arrays and Dictionaries - dimensions

--- effective_javascript/array-dict.js
+++ effective_javascript/array-dict.js
@@ -1,12 +1,20 @@
-function someDictionary() { } 
+function NaiveDict() { } 
 
-someDictionary.prototype.count = function() { 
-	var i = 0; 
-	var name; 
-	for (name in dict) {
-		i++ 
+NaiveDict.prototype.count = function() {
+	var i = 0, name; 
+	for (name in dict) { 
+		i++; 
 	} 
-return i; 
-} 
+	console.log(i);
+}; 
 
+NaiveDict.prototype.toString = function() {
+	console.log("[object NaiveDict]"); 
+}; 
+
+var dict = new NaiveDict();
+
+dict.khu = 34, dict.enosh = 24, dict.osa = 62
+
+dict.count(); 
 
