d6654329d2b40108c5487ff5df8a968a2e52fa59
hoodielive <hoodielive@gmail.com>
2018-04-25 18:54:30 -0400

    Object computed property names

---
+++ Vince-Nick/obj-computed-property.js
@@ -0,0 +1,15 @@
+var occ = 'artist'
+
+var propObj = {
+	id: 'Artisty',
+	name: 'Nick', 
+	age: 27,
+	cache: {}, 
+	[occ]: {
+		years: 11, 
+		interests: ['herbs', 'travel', 'ableton', 'tai chi'] 
+      } 
+}
+
+console.log(propObj.artist)
+
