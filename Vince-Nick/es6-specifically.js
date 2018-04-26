e92cf3ea06142a945e856be1b09f68277b1ca5f6
hoodielive <hoodielive@gmail.com>
2018-04-26 13:08:47 -0400

    New Syntax relative to changing object values

--- Vince-Nick/es6-specifically.js
+++ Vince-Nick/es6-specifically.js
@@ -6,8 +6,9 @@
 
 // obj = {}; // will give you a type error, but you could make the value itself immutable by freezing it
 
-const obj = Object.freeze({}); 
+// const obj = Object.freeze({}); 
+const obj = {};  
 
 obj.key = 42; 
-
-console.log(obj) 
+obj.key = 7; 
+console.log(obj.key) 
