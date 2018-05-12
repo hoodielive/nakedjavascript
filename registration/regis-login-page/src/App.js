2877aac35231cdb1094c9677c0bee9d43100c6ad
hoodielive <hoodielive@gmail.com>
2018-05-11 23:05:47 -0400

    React Registration for create-react-apps

---
+++ registration/regis-login-page/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+               
+          <h1>Welcome to Apokueo</h1>
+        <p>
+          Welcome to this Application, lets get started with Registration.</p>
+          
+      </div>
+    );
+  }
+}
+
+export default App;
