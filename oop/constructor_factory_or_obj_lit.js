f7b6ed02ce730a1cc4c3ab0d9fdfe0a9ef4d301d
hoodielive <hoodielive@gmail.com>
2018-05-15 16:08:01 -0400

    factory or constructor

---
+++ oop/constructor_factory_or_obj_lit.js
@@ -0,0 +1,20 @@
+// obj literal 
+const circle = { 
+	radius: 1, 
+	location: {
+		x: 1, 
+		y: 1
+	}, 
+
+	draw: function() { 
+		console.log('draw')
+	}
+}; 
+
+// or Factory ? 
+function createCircle() { 
+	return {
+		radius, // if the var name and value are the same, then just add name. Equiv to createCircle(radius) {
+			// const circle = {
+				// radius: radius
+		// 	} 
