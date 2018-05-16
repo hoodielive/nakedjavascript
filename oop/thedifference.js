3bb7460f25a1a6cd5d56dff68940010d5f564351
hoodielive <hoodielive@gmail.com>
2018-05-15 23:18:34 -0400

    making minor changes with regards to factoring

---
+++ oop/thedifference.js
@@ -0,0 +1,24 @@
+var Anewobj = { 
+    baseSalary: 40000, 
+    overTime: 10, 
+    rate: 20, 
+    getWage: function(radius) { 
+        return baseSalary * (overTime * rate)
+    } 
+} 
+
+objis = new Anewobj
+
+
+function Anewobj(radius) {
+    return { 
+        radius, 
+        baseSalary: 40000, 
+        overTime: 10, 
+        rate: 20, 
+        getWage: function(radius) { 
+            return baseSalary * (overTime * rate)
+        } 
+} 
+
+Encapsulation
