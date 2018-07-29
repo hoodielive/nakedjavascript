ecd01c25d5291ccec5389574a965772d7ea6941d
hoodielive <hoodielive@gmail.com>
2018-07-29 17:44:42 -0400

    forEach freakiness

--- exercises/arrayMethods.js
+++ exercises/arrayMethods.js
@@ -1,7 +1,10 @@
 const array1 = ['i', 'am', 'an', 'array'] 
 
-const arrayobj = [{ givenNames: 'veggie burger', surName: 'burger', status: 'single', age: 19, occupation: 'aggravating people'},
-							{ givenNames: 'pineapple burger', surName: 'pineapple', status: 'married', age: 27, occupation: 'lifting up people'}]
+const arrayobj = [{ givenNames: 'Mr. Veggie Burger', surName: 'burger', status: 'single', age: 19, occupation: 'aggravating people'},
+							{ givenNames: 'Mr. Pineapple Burger', surName: 'pineapple', status: 'married', age: 27, occupation: 'lifting up people'}]
+
+const arrayobj2 = [{ givenNames: 'Ms. Chocolate Cake', surName: 'cake', status: 'single', age: 22, occupation: 'twerking'},
+							{ givenNames: 'Ms. Fascinating Flower', surName: 'flower', status: 'married', age: 37, occupation: 'painting'}]
 
 const fraeyah = array1.forEach(function(elem) {
 	console.log(elem);
@@ -18,6 +21,11 @@ function elijah(elem, index, array) {
 	return;
 };
 
+function rebekah(elem, index, array) {
+	console.log('[array_index[' + index + elem + ']');
+};
+
 console.log(fraeyah); 
 console.log(klaus);
 array1.forEach(elijah); 
+arrayobj2.forEach(rebekah);
