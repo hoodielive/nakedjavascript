18c6de97694a9530b97f421cac23a9dc25a8fce8
hoodielive <hoodielive@gmail.com>
2018-07-29 17:22:57 -0400

    array methods

---
+++ exercises/arrayMethods.js
@@ -0,0 +1,23 @@
+const array1 = ['i', 'am', 'an', 'array'] 
+const arrayobj = [{ givenNames: 'veggie burger', surName: 'burger', status: 'single', age: 19, occupation: 'aggravating people'},
+							{ givenNames: 'pineapple burger', surName: 'pineapple', status: 'married', age: 27, occupation: 'lifting up people'}]
+
+
+const fraeyah = array1.forEach(function(elem) {
+	console.log(elem);
+	return;
+}); 
+
+const klaus = arrayobj.forEach(function(elem) { 
+	console.log(elem);
+	return;
+}); 
+
+function elijah(elem, index, array) {
+	console.log('a[' + index + '] = ' + elem); 
+	return;
+}
+
+console.log(fraeyah); 
+console.log(klaus);
+array1.forEach(elijah); 
