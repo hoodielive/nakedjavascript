c9ebf68855652d3ecda5d1d435bd69e9e0c53353
hoodielive <hoodielive@gmail.com>
2018-07-29 16:50:57 -0400

    prototypal slicing is good

--- exercises/prototypal.slicing.js
+++ exercises/prototypal.slicing.js
@@ -8,7 +8,32 @@ function list2() {
 	console.log(arr2);
 };
 
-arrayLike = [1,2,3,4]; 
+function list3() {
+	const arr3 = [].slice.call(arguments); 
+	console.log(arr3); 
+};
 
-list(arrayLike);
+
+function checkArr() { 
+	if (list == list) {
+		list(arrayLike); 
+	} 
+	else {
+		console.log('list is not present');
+	} 
+};
+
+let unboundSlice = Array.prototype.slice; 
+let slice = Function.prototype.call.bind(unboundSlice);
+
+function list4() {
+	console.log(slice(arguments)); 
+};
+
+arrayLike = [1,2,3,4]; 
+arrayLike2 = ['this', 'is', 'just', 'so', 'I', 'would', 'have', 'more', 'interesting', 'data'];
+
+checkArr(); 
 list2(arrayLike); 
+list3(arrayLike);
+list4(arrayLike2); 
