fd0d16e743e92450982f73a77132e89fc233b023
hoodielive <hoodielive@gmail.com>
2018-07-30 17:48:04 -0400

    prototypal inheritance

---
+++ exercises/prototypal_inheritance.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node 
+
+/** say what you want 
+ *
+ * but in Javascript we do NOT have classes 
+ * what we do have is objects 
+ **/
+
+// every object has a function constructor that was used to create the object 
+let x = {}; 
+console.log(x.toString); 
