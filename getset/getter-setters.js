7b7eb864b2503ba42cd0edf0b4d7c9a88491061d
hoodielive <hoodielive@gmail.com>
2018-09-05 23:23:17 -0400

    wrote a test to validate a string

--- getset/getter-setters.js
+++ getset/getter-setters.js
@@ -6,6 +6,8 @@ const person = {
         return `${person.firstName} ${person.lastName} `
     },
     set fullName(value) {
+       // make sure that you have a valid string 
+        if (typeof value !== 'string') return; 
         const parts = value.split(' ')
         this.firstName = parts[0];
         this.lastName = parts[1]; 
