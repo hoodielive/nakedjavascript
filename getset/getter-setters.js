d486aa1d0516d6521b05dfd51093601dd48a6ece
hoodielive <hoodielive@gmail.com>
2018-09-05 23:18:28 -0400

    expand getter and setter

--- getset/getter-setters.js
+++ getset/getter-setters.js
@@ -4,10 +4,15 @@ const person = {
     lastName: 'Something',
     get fullName() {
         return `${person.firstName} ${person.lastName} `
+    },
+    set fullName(value) {
+        const parts = value.split(' ')
+        this.firstName = parts[0];
+        this.lastName = parts[1]; 
     }
-}
+}; 
 
-person.fullName = 'Jon Doe';
+person.fullName = 'Jon Doe'; // when the setter is set, then you can 'set' the name as jon doe
 
 // getters => access properties
 // setters => change (mutate) them
