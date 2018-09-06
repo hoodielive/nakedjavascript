6ee583619077f728e6835186e38fdde985b2f489
hoodielive <hoodielive@gmail.com>
2018-09-05 20:18:42 -0400

    booleans

---
+++ es64humans/boolean.js
@@ -0,0 +1,21 @@
+console.log("" == false)
+
+console.log("" === false)
+
+console.log(!!"" == false)
+
+console.log(!!"" === false)
+
+b = 'silence'
+
+//console.log(b.p.constructor()) console.log(b.p.toString()) console.log(b.p.valueOf())
+
+// checking to see if a variable exists 
+
+let someVar = 123; 
+
+if (someVar) {
+  result = yes; 
+  return result; 
+}
+
