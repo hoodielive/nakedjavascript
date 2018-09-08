0448874aec206124546567b319372ceadbe93fe0
hoodielive <hoodielive@gmail.com>
2018-09-08 16:29:07 -0400

    Working with Generators

--- helper functions/generators/_generators.js
+++ helper functions/generators/_generators.js
@@ -6,8 +6,12 @@ function* numbers() {
 
 console.log(numbers())
 
-// function *nummer() { }
+// function *nummer() { } is another way to create a generator 
 
 const gen = numbers(); 
+
 gen.next(); // true
-gen.next(); // false 
+
+gen.next(); // false -> it would be true if you get rid of yield  
+
+
---
+++ helper functions/generators/git-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# script because I'm lazy lol 
+
+git add -A 
+
+sleep 5
+
+git commit -m "$1"
+
+sleep 5
+
+gp
