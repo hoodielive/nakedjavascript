554e9d0ba2c24e2f39c45853b36d2b529a79e4f1
hoodielive <hoodielive@gmail.com>
2018-10-10 17:35:40 -0400

    blocks, private data

---
+++ exercises/next/src/block.js
@@ -0,0 +1,20 @@
+// use a free-standing block to keep a variable private
+
+let read, write; 
+{
+  let data = {}; 
+  write = function(key, val) { 
+    data[key] = val;  
+  }
+
+  read = function(key) {
+    return data[key];  
+  }
+}
+
+write('message', 'welcome to ES6!'); 
+
+read('message'); 
+
+//console.log(data); would generate an error because you would be referencing an object outside of a private block
+
