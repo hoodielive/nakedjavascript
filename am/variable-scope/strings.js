64643fb5544bfbbd490f45d85c55c2dd9c09ddc5
hoodielive <hoodielive@gmail.com>
2018-11-24 10:34:57 -0500

    javascript practicum because its cool

---
+++ am/variable-scope/strings.js
@@ -0,0 +1,3 @@
+let name = 'Ego is the Enemy'
+
+console.log(name);
---
+++ am/variable-scope/temp-conversion.js
@@ -0,0 +1,11 @@
+let farenheit = 50; // 0c - 273.15k
+
+/** calculate celisus and store in variable */
+let celsius = (farenheit - 32) * 5/9; 
+
+/** convert to Kelvin */ 
+let kelvin = (farenheit + 459.67) * 5/9;
+
+console.log(celsius);
+
+console.log(kelvin);
