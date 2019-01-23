ee880b5c6d8e771c12bd0853870324c88727f430
hoodielive <hoodielive@gmail.com>
2019-01-23 13:46:29 -0500

    array tips and tricks

---
+++ misc/eachfor.js
@@ -0,0 +1,5 @@
+const array = [1,2,3,4,5,6]; 
+
+array.forEach(function(current_value, index, array) {
+  console.log(`At index ${index} in array ${array} where current value is ${current_value}`);
+}); 
---
+++ misc/index.js
@@ -0,0 +1,7 @@
+let index = 0; 
+const array = [1,2,3,4,5,6];
+
+while (index < array.length) {
+  console.log("The array is printing from index", array[index]); 
+  index++ 
+}
