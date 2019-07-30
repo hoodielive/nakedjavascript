dc55460c46f31cb38156aa72823b706d229ad7c7
hoodielive <hoodielive@gmail.com>
2019-07-30 18:13:11 -0400

    forgot to check this in

---
+++ es6 4 humans/session_01/orp.js
@@ -0,0 +1,8 @@
+const readline = require('readline')
+
+function surelyTheyCanSee() {
+    let arg = readline('What is your name willis? ')
+    console.log('its a shame ' + arg)
+}
+
+surelyTheyCanSee()
