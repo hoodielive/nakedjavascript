e810a68a19eca899448ea5fc5561f6524e2edfcb
hoodielive <hoodielive@gmail.com>
2019-08-10 00:02:24 -0400

    of and in changes return

--- effective javascript/doitagain.js
+++ effective javascript/doitagain.js
@@ -1,9 +1,8 @@
-dict = { fname: 'Nabi', lname: 'Sod' }
-
-people = [] 
+dict = { fname: 'Nabi', lname: 'Sod' };
+people = [];
 
 for (let name in dict) {
-  people.push(name + ' : ' + dict[name])
+  people.push(name + ' : ' + dict[name]);
   console.log(people);
 }
 
